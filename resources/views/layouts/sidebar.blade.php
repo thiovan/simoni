2bfe368f51869baaa4e2f321c40ff227bc6286c1
Thio Van <thiovan99@gmail.com>
2024-11-10 23:23:11 +0700

    WIP sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div x-show="open" class="relative z-40 md:hidden" x-ref="dialog" aria-modal="true">
+<div x-show="open" class="position-relative z-40 d-md-none" x-ref="dialog" aria-modal="true">
 
     <div x-show="open" x-transition:enter="transition-opacity ease-linear duration-300"
         x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
@@ -87,52 +87,58 @@
 </div>
 
 <!-- Static sidebar for desktop -->
-<div class="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
-    <div class="absolute right-0 w-2 h-full bg-sidebar-alt"></div>
+<div class="d-none d-md-flex position-fixed start-0 top-0 bottom-0 w-25">
     <!-- Sidebar component, swap this element with another sidebar if you like -->
-    <div class="flex flex-col flex-grow pt-5 overflow-y-auto border-r border-gray-200 bg-sidebar">
-        <div class="flex flex-col items-center flex-shrink-0 px-4">
-            <img class="w-auto h-16" src="{{ asset('images/pemkot.png') }}" alt="logo pemkot semarang">
-            <div class="mt-2 text-sm text-center text-white font-franklin-gothic font-border">
-                <h3>PEMERINTAH KOTA<br>SEMARANG</h3>
+    <div class="d-flex flex-column align-items-center flex-grow-1 pt-5 bg-danger bg-gradient">
+        <div class="d-flex flex-column align-items-center px-4">
+            <img class="img-fluid w-25" src="{{ asset('images/pemkot.png') }}" alt="logo pemkot semarang">
+            <div class="mt-2 fs-5 text-center text-white">
+                PEMERINTAH KOTA<br>SEMARANG
             </div>
         </div>
-        <div class="flex flex-col flex-grow p-2 mt-10">
-            <nav class="grid grid-cols-2 px-2 pb-4 gap-x-3 gap-y-8">
+        <div class="d-flex flex-column flex-grow-1 p-2 mt-4">
+            <nav>
 
-                <div class="hvr-pulse-shrink">
-                    <a href="dashboard"
-                        class="flex items-center justify-center px-2 py-2 rounded-md group hover:bg-red-700 {{ Route::currentRouteName() == 'dashboard' ? 'bg-red-700' : '' }}">
-                        <img class="w-16 drop-shadow" src="{{ asset('icons/home.svg') }}">
-                    </a>
+                <div class="row">
+                    <div class="col">
+                        <a href="dashboard"
+                            class="d-flex align-items-center justify-content-center p-2 {{ Route::currentRouteName() == 'dashboard' ? 'bg-danger' : '' }}">
+                            <img class="img-fluid w-50" src="{{ asset('icons/home.svg') }}">
+                        </a>
+                    </div>
+
+                    <div class="col">
+                        <a href="report"
+                            class="d-flex align-items-center justify-content-center p-2 {{ Route::currentRouteName() == 'report' ? 'bg-danger' : '' }}">
+                            <img class="img-fluid w-50" src="{{ asset('icons/envelop.svg') }}">
+                        </a>
+                    </div>
                 </div>
 
-                <div class="hvr-pulse-shrink">
-                    <a href="report"
-                        class="flex items-center justify-center px-2 py-2 rounded-md group hover:bg-red-700 {{ Route::currentRouteName() == 'report' ? 'bg-red-700' : '' }}">
-                        <img class="w-16 drop-shadow" src="{{ asset('icons/envelop.svg') }}">
-                    </a>
+                <div class="row">
+                    <div class="col">
+                        <a href="keyword"
+                            class="d-flex align-items-center justify-content-center p-2 {{ Route::currentRouteName() == 'keyword' ? 'bg-danger' : '' }}">
+                            <img class="img-fluid w-50" src="{{ asset('icons/gps.svg') }}">
+                        </a>
+                    </div>
+
+                    <div class="col">
+                        <a href="type"
+                            class="d-flex align-items-center justify-content-center p-2 {{ Route::currentRouteName() == 'type' ? 'bg-danger' : '' }}">
+                            <img class="img-fluid w-50" src="{{ asset('icons/checklist.svg') }}">
+                        </a>
+                    </div>
                 </div>
 
-                <div class="hvr-pulse-shrink">
-                    <a href="keyword"
-                        class="flex items-center justify-center px-2 py-2 rounded-md group hover:bg-red-700 {{ Route::currentRouteName() == 'keyword' ? 'bg-red-700' : '' }}">
-                        <img class="w-16 drop-shadow" src="{{ asset('icons/gps.svg') }}">
-                    </a>
-                </div>
-
-                <div class="hvr-pulse-shrink">
-                    <a href="type"
-                        class="flex items-center justify-center px-2 py-2 rounded-md group hover:bg-red-700 {{ Route::currentRouteName() == 'type' ? 'bg-red-700' : '' }}">
-                        <img class="w-16 drop-shadow" src="{{ asset('icons/checklist.svg') }}">
-                    </a>
-                </div>
-
-                <div class="hvr-pulse-shrink">
-                    <a href="category"
-                        class="flex items-center justify-center px-2 py-2 rounded-md group hover:bg-red-700 {{ Route::currentRouteName() == 'category' ? 'bg-red-700' : '' }}">
-                        <img class="w-16 drop-shadow" src="{{ asset('icons/tools.svg') }}">
-                    </a>
+                <div class="row">
+                    <div class="col">
+                        <a href="category"
+                            class="d-flex align-items-center justify-content-center p-2 {{ Route::currentRouteName() == 'category' ? 'bg-danger' : '' }}">
+                            <img class="img-fluid w-50" src="{{ asset('icons/tools.svg') }}">
+                        </a>
+                    </div>
+                    <div class="col"></div>
                 </div>
 
             </nav>
